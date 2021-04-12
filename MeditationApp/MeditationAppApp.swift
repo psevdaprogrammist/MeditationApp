c3389533262850c54726a7545a6e0990fc720ee5
Egor <korchagin.egorka1@gmail.com>
2021-04-12 15:05:33 +1000

    Initial Commit

---
+++ MeditationApp/MeditationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeditationAppApp.swift
+//  MeditationApp
+//
+//  Created by Egor Korchagin on 12.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct MeditationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
